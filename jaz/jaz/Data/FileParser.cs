65ef4f6840e51a604dbfe9b8c24cd1df965b6c52
Dennis D. Silva <dsilva609@gmail.com>
2015-01-25 01:36:40 -0500

    added fileparser

---
+++ jaz/jaz/Data/FileParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace jaz.Data
+{
+	public class FileParser
+	{
+		public String[] ReadData(string filename)
+		{
+			return File.ReadAllLines(filename);
+
+		}
+	}
+}
---
+++ jaz/jaz/MainWrapper.cs
@@ -0,0 +1,22 @@
+﻿using jaz.Data;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace jaz
+{
+	public class MainWrapper
+	{
+		public static void Main(string[] args)
+		{
+			var parser = new FileParser();
+
+			var data = parser.ReadData(Assembly.GetExecutingAssembly().Location.Replace("\\bin\\Debug\\jaz.exe", "") + @"\\Resources\\foo.jaz");//@"..\..\Resources\foo.jaz");
+
+
+			Console.WriteLine(data.Length);
+
+			Console.WriteLine(new Uri(@"Resources\foo.jaz", UriKind.Relative).ToString());
+		}
+	}
+}

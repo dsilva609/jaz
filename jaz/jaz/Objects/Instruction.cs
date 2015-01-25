0c159de5a4c99529a4da1fd4044cc5be964b61d4
Dennis D. Silva <dsilva609@gmail.com>
2015-01-25 15:53:16 -0500

    added comment for instruction property

--- jaz/jaz/Objects/Instruction.cs
+++ jaz/jaz/Objects/Instruction.cs
@@ -4,6 +4,6 @@
 	{
 		public string Name { get; set; }
 		public string Value { get; set; }
-
+		//add property for InstructionSetValue from a new InstructionSet object that has public const string properties
 	}
 }

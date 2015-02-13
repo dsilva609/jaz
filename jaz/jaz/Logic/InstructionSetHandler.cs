e2bd7b2b1ba95d08ab5ff616c9f37b2a11be0092
dsilva609 <dsilva609@gmail.com>
2015-02-13 18:48:33 -0500

    moved switch to InstructionSetHandler

--- jaz/jaz/Logic/InstructionSetHandler.cs
+++ jaz/jaz/Logic/InstructionSetHandler.cs
@@ -16,6 +16,139 @@ namespace jaz.Logic
 			this._symbolTable = new Dictionary<string, object>();
 		}
 
+		public void DetermineAndExecuteInstructionOperation(Instruction item)
+		{
+			switch (item.Command)
+			{
+				default:
+					break;
+
+				case InstructionSet.Addition:
+					this.Addition();
+					break;
+
+				case InstructionSet.AND:
+					this.AND();
+					break;
+
+				case InstructionSet.Begin:
+					this.Begin();
+					break;
+
+				case InstructionSet.Call:
+					this.Call();
+					break;
+
+				case InstructionSet.Copy:
+					this.Copy();
+					break;
+
+				case InstructionSet.Division:
+					this.Division();
+					break;
+
+				case InstructionSet.End:
+					this.End();
+					break;
+
+				case InstructionSet.Equal:
+					this.Equal();
+					break;
+
+				case InstructionSet.GoFalse:
+					this.GoFalse();
+					break;
+
+				case InstructionSet.GoTo:
+					this.GoTo();
+					break;
+
+				case InstructionSet.GoTrue:
+					this.GoTrue();
+					break;
+
+				case InstructionSet.Greater:
+					this.Greater();
+					break;
+
+				case InstructionSet.GreaterOrEqual:
+					this.GreaterOrEqual();
+					break;
+
+				case InstructionSet.Halt:
+					this.Halt();
+					break;
+
+				case InstructionSet.Label:
+					this.Label();
+					break;
+
+				case InstructionSet.Lesser:
+					this.Lesser();
+					break;
+
+				case InstructionSet.LesserOrEqual:
+					this.LesserOrEqual();
+					break;
+
+				case InstructionSet.LValue:
+					this.LValue(item.Value);
+					break;
+
+				case InstructionSet.Multuplication:
+					this.Multuplication();
+					break;
+
+				case InstructionSet.NOT:
+					this.NOT();
+					break;
+
+				case InstructionSet.NotEqual:
+					this.NotEqual();
+					break;
+
+				case InstructionSet.OR:
+					this.OR();
+					break;
+
+				case InstructionSet.Pop:
+					this.Pop();
+					break;
+
+				case InstructionSet.Print:
+					this.Print();
+					break;
+
+				case InstructionSet.Push:
+					this.Push(item);
+					break;
+
+				case InstructionSet.Remainder:
+					this.Remainder();
+					break;
+
+				case InstructionSet.Return:
+					this.Return();
+					break;
+
+				case InstructionSet.RValue:
+					this.RValue(item.Value);
+					break;
+
+				case InstructionSet.Show:
+					this.Show(item.Value);
+					break;
+
+				case InstructionSet.Subtraction:
+					this.Subtraction();
+					break;
+
+				case InstructionSet.ReplaceTop:
+					this.ReplaceTop();
+					break;
+			}
+		}
+
 		#region Stack Manipulation
 
 		public virtual void Push(object item)

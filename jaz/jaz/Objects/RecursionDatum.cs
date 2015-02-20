388b7b98c3fd79c4b5b35d3f66d29dc085c1b817
dsilva609 <dsilva609@gmail.com>
2015-02-20 13:32:55 -0500

    added recursion datum object

---
+++ jaz/jaz/Objects/RecursionDatum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jaz.Objects
+{
+	public class RecursionDatum
+	{
+		public string RecursionID { get; set; }
+
+		public int CallsRemaining { get; set; }
+
+		public int RecursiveCalls { get; set; }//probably not needed
+
+		public Guid RecursiveReturnValue { get; set; }
+
+		public Guid OriginalReturnValue { get; set; }
+
+		public Stack<int> RecursiveValueStorage { get; set; }//////////////////how should this be used?
+
+		public RecursionDatum()
+		{
+			CallsRemaining = 0;
+		}
+	}
+}

44418d9fb65e15c45250838973c1bc69da01cbf3
jstirling <jonathan.r.stirling@gmail.com>
2015-02-26 14:47:52 -0500

    Update RecursionDatum.cs
    comment cleanup

--- jaz/jaz/Objects/RecursionDatum.cs
+++ jaz/jaz/Objects/RecursionDatum.cs
@@ -9,20 +9,15 @@ namespace jaz.Objects
 
 		public string RecursionFunctionName { get; set; }
 
-		//public int CallsRemaining { get; set; }//probably not needed
-
-		//public int RecursiveCalls { get; set; }//probably not needed
-
 		public Guid RecursiveReturnValue { get; set; }
 
 		public Guid OriginalReturnValue { get; set; }
 
-		public Dictionary<string, Stack<int>> RecursiveValueStorage { get; set; }//////////////////how should this be used?
+		public Dictionary<string, Stack<int>> RecursiveValueStorage { get; set; }
 
 		public RecursionDatum()
 		{
 			RecursiveValueStorage = new Dictionary<string, Stack<int>>();
-			//CallsRemaining = 0;
 		}
 	}
 }
